fe938dc27e6622b104725ce1c72aee64da46ff98
mf-yechan-do <Yechan.do@mofl.ai>
2021-11-09 04:51:32 +0900

    211109_v4

--- mofl-app/component/home.component/banner.tsx
+++ mofl-app/component/home.component/banner.tsx
@@ -26,7 +26,7 @@ export default class Banner extends Component<Props> {
         </Content>
         <div>
       <Video style={{paddingBottom:"180px"}}
-      height="auto" autoPlay muted loop playsinline preload poster="/images/mofl_video.jpg">
+      height="auto" autoPlay muted loop playsinline preload poster="/images/mofl_video.gif">
         <source src="/images/mofl_video.webm" type="video/webm"></source>
       <source src="/images/mofl_video.mp4" type="video/mp4"></source>
       </Video>
