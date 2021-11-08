fa3f5e08afa7fa70ad3088316024a1e537311f6a
GitBub Name <E-mail>
2021-11-08 14:44:41 +0900

    211108_v8

--- mofl-app/component/home.component/banner.tsx
+++ mofl-app/component/home.component/banner.tsx
@@ -26,11 +26,11 @@ export default class Banner extends Component<Props> {
         </Content>
         <div>
       <Video
-      height="auto"  muted autoplay loop playsinline>
+      height="auto"  muted="muted" autoplay="autoplay" loop="loop" playsinline="">
       <source src="/images/1pages.mp4" type="video/mp4"/>
       </Video>
       <Video   
-      height="auto" muted="muted" autoplay="autoplay" loop="loop" playsinline="playsinline">
+      height="auto" autoPlay muted loop playsinline>
       <source src="/images/3pages.mp4" type="video/mp4"/>
       </Video>
       </div>
